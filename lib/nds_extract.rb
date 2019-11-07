246655da11b614825fdeb702d9cea6c97a747f02
Ben Miriello <benmiriello@gmail.com>
2019-11-07 19:14:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,7 @@ def list_of_directors(source)
   list = []
   count = 0
   while count < source.length do
-    list << "#{source[count][:name]}"
+    list << source[count][:name]
     count += 1
   end  
   list
